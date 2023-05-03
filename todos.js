9cc927051eadc6f1e636f19d928d55c13b3116b5
paras9sid <sidharthjainp9@gmail.com>
2023-05-03 20:49:34 +0530

    dekete functionality with index and unkn index add

--- todos.js
+++ todos.js
@@ -12,10 +12,17 @@ while (input !== "quit" && input !== "q") {
   } else if (input === "new") {
     const newTodo = prompt("Ok,What is new todo?");
     todos.push(newTodo);
-    console.log(`${newTodo} added to the list!`);
-//   }else if(){
-
-//   }
+    console.log(`${newTodo}: Added to the list!`);
+  } else if (input === "delete") {
+    const index = parseInt(prompt("Ok, enter index to delete todo!"));
+    // const index = parseInt(indexStr);
+    if (!Number.isNaN(index)) {
+      const deleted = todos.splice(index, 1);
+      console.log(`Ok , deleted: ${deleted[0]}`);
+    } else {
+      console.log("Unknow Index");
+    }
+  }
   input = prompt("What would you like to do?");
 }
 console.log("Quit the app");
